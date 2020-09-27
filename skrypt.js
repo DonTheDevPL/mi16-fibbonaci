f2f395121d1bf151c66b6240b0a438b87079dc50
Michał <33964835+DonTheDevPL@users.noreply.github.com>
2020-09-27 17:10:13 +0200

    ez

--- skrypt.js
+++ skrypt.js
@@ -1,14 +1,20 @@
-document.getElementById('przycisk').addEventListener('click',znajdzFibb())
+document.getElementById('przycisk').addEventListener('click', znajdzFibb);
 
-function znajdzFibb(){
-    let numer = document.getElementById('numer').value; 
-    let wynik = fibbonacci(numer);
-    let wynikOutput = document.querySelector("#wynik");
-    wynikOutput.innerHTML = wynik;
+function znajdzFibb() {
+	let numer = document.getElementById('numer').value;
+	if (numer < 0) {
+		let wynik = 'Błąd! Ciąg fibbonaciego występuje od 0';
+		let wynikOutput = document.querySelector('#wynik');
+		wynikOutput.innerHTML = wynik;
+	}
+	else {
+		let wynik = fibbonacci(numer);
+		let wynikOutput = document.querySelector('#wynik');
+		wynikOutput.innerHTML = wynik;
+	}
 }
-function fibbonacci(numer){
-    let wynik;
-    if(numer == 0) return 0;
-    if(numer == 1) return 1;
-    return (fibbonacci(numer-1)+fibbonacci(numer-2));
+function fibbonacci(numer) {
+	if (numer == 0) return 0;
+	if (numer == 1) return 1;
+	return fibbonacci(numer - 1) + fibbonacci(numer - 2);
 }
